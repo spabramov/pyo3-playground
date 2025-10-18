66c953bb773b400d04032ced7cfaf71dbf304dbd
Sergey Abramov <s.abramov@glowbyte.io>
2025-10-18 22:34:28 +0300

    basic python subproject structure

---
+++ python/pyo3_playground/__init__.py
@@ -0,0 +1,5 @@
+from .pyo3_playground import *
+
+__doc__ = pyo3_playground.__doc__
+if hasattr(pyo3_playground, "__all__"):
+    __all__ = pyo3_playground.__all__
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("I'm using the library: {:?}", "aboba");
+}
